bb056c34b19a8a5dcf2771957852d147382874af
Roopvirk6 <60120994+Roopvirk6@users.noreply.github.com>
2020-07-22 16:33:32 -0400

    Update MapsActivity.java

--- app/src/main/java/com/example/finalassignment_ikroop_c0774174/MapsActivity.java
+++ app/src/main/java/com/example/finalassignment_ikroop_c0774174/MapsActivity.java
@@ -341,7 +341,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 break;
 
             case R.id.btn_clear:
-                mMap.clear();
+               // mMap.clear();
                 break;
 
         }
